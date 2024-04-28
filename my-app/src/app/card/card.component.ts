28631650b4bfb0b5db039fc8bb75d49e5bdce190
racerf01 <racerf01@mail.ru>
2024-04-28 15:49:47 +0200

    v1.0

--- my-app/src/app/card/card.component.ts
+++ my-app/src/app/card/card.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
   @Input() card: any;
