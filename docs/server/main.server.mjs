97e8ec78b86f702d1b3aee3f9c071a25f5050f0d
racerf01 <racerf01@mail.ru>
2024-05-10 10:59:27 +0200

    test build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HKL7NR5D.mjs";import"./chunk-HR72KKFO.mjs";import"./chunk-JKREWEG4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-MYJIKFTO.mjs";import"./chunk-2R2GIYDS.mjs";import"./chunk-NHQPTIUJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
