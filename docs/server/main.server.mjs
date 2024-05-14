f15c256a9a30f95d7dd2f571ba2b013a16744ccc
racerf01 <racerf01@mail.ru>
2024-05-14 22:16:43 +0200

    build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-W2INHHZS.mjs";import"./chunk-U4H2ZFRD.mjs";import"./chunk-4FCOSBAE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-UFDHXXDE.mjs";import"./chunk-U4H2ZFRD.mjs";import"./chunk-4FCOSBAE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
